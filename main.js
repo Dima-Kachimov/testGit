fbd70b6ccf0078a3c3b9eacf8a8418f5ba272871
Kachimov Dmytro <romanov.dima.13.11.98@gmail.com>
2025-08-23 17:50:21 +0300

    First aplication

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-let isChecked = true;
-let isClose = false;
+const numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const lastMovie = prompt("Один из последних просмотренных фильмов?");
+  const ratingLastMovie = prompt("На сколько вы оцениваете его?");
+  personalMovieDB.movies[lastMovie] = ratingLastMovie;
+}
+
+console.log(personalMovieDB);
